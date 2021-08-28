de3fe14079ed30dee9e2421c1aed3a6f3ad47421
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-08-28 17:10:34 -0300

    Criando o controller de autenticação

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,19 @@
 module.exports.index = function(application, req, res){
-    res.render('index');
+    res.render('index', {validacao: {}});
+}
+
+module.exports.autenticar = function(application, req, res){
+
+    var dadosForm = req.body;
+    
+    req.assert("usuario", "Usuário não pode ficar em branco").notEmpty();
+    req.assert("senha", "Nome não pode ficar em branco").notEmpty();
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render("index", {validacao: erros});
+        return;
+    }
+
+    res.send("tudo ok para criar sessão");
 }
